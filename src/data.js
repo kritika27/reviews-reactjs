50bbbbde04c81322343203498c6bb38fb2e7886a
kritika <kritikasri27@gmail.com>
2021-02-01 14:06:26 +0530

    Added data containing reviews of a restaurant given by few people

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Susan Smith",
+    job: "Web Developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quas ipsa debitis nesciunt culpa eos omnis voluptas, neque unde asperiores iste voluptates possimus explicabo porro ducimus adipisci! Minus, soluta iste?",
+  },
+  {
+    id: 2,
+    name: "Anna Johnson",
+    job: "Designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quas ipsa debitis nesciunt culpa eos omnis voluptas, neque unde asperiores iste voluptates possimus explicabo porro ducimus adipisci! Minus, soluta iste?",
+  },
+  {
+    id: 3,
+    name: "Peter Jones",
+    job: "Journalist",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non doloremque alias dicta laboriosam architecto sequi fuga veniam asperiores. Dolor inventore facere sit id illum veritatis?",
+  },
+  {
+    id: 4,
+    name: "Bill Anderson",
+    job: "Tennis Player",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non doloremque alias dicta laboriosam architecto sequi fuga veniam asperiores. Dolor inventore facere sit id illum veritatis?",
+  },
+];
+
+export default reviews;
